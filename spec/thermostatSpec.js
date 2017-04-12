92e9042c9f74d090446efddb0a2e829349c2e1df
Simba14 <seanblundell@gmail.com>
2017-04-12 11:32:42 +0100

    Minimum temperature is 10 degrees

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -22,6 +22,17 @@ describe('Thermostat', function() {
       thermostat.decrease(4);
       expect(thermostat.temp).toEqual(16);
     });
+
+    it('temperature cannot be set below 10 degrees', function() {
+      thermostat.decrease(11);
+      expect(thermostat.temp).toEqual(10);
+    });
+
+    it('throws a message if target temperature is below 10 degrees', function(){
+      console.log = jasmine.createSpy("log")
+      thermostat.decrease(11);
+      expect(console.log).toHaveBeenCalledWith('Temperature has been set to the minimum of 10 degrees');
+    });
   });
 
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -9,5 +9,10 @@ Thermostat.prototype.increase = function (degrees) {
 };
 
 Thermostat.prototype.decrease = function (degrees) {
-  this.temp -= degrees;
+  if((this.temp - degrees) < 10) {
+    this.temp = 10;
+    return console.log('Temperature has been set to the minimum of 10 degrees');
+  } else {
+    this.temp -= degrees;
+  };
 };
